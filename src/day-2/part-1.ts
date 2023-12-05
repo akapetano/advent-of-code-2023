c10c6d25d3e9f062453db8ca4379e588752095f4
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2023-12-05 13:45:52 +0100

    solve day 2, part 1

--- src/day-2/part-1.ts
+++ src/day-2/part-1.ts
@@ -1,46 +1,52 @@
 import { join } from "path";
 import { parseInput } from "../utils/input";
 import { sum } from "../utils/number";
+import { InputType } from "types";
 
 const path = join(__dirname, "input.txt");
-const input = parseInput(path, { split: true }) as string[];
+const inputData = parseInput(path, { split: true }) as string[];
 
 const maxCubes = { red: 12, green: 13, blue: 14 } as const;
 
-function getPossibleGamesIds(input: string): number {
-  const [gameId, gameContent] = input.split(": ");
-  const content = gameContent.split("; ");
-  const [__, id] = gameId.split(" ");
-  const valueColorPairString = content.map((turn) => turn.split(", ")).flat();
-  const keyValuePairs = valueColorPairString
-    .map((pair) => {
-      const [number, color] = pair.split(" ");
-      return { [color]: Number(number) };
-    })
-    .flat();
+export type CubeRevealTuple = [value: number, color: "red" | "green" | "blue"];
 
-  const red = keyValuePairs
-    .filter((pair) => Object.keys(pair)[0] === "red")
-    .map((pair) => Object.values(pair)[0])
-    .reduce((acc, curr) => acc + curr, 0);
-  const blue = keyValuePairs
-    .filter((pair) => Object.keys(pair)[0] === "blue")
-    .map((pair) => Object.values(pair)[0])
-    .reduce((acc, curr) => acc + curr, 0);
-  const green = keyValuePairs
-    .filter((pair) => Object.keys(pair)[0] === "green")
-    .map((pair) => Object.values(pair)[0])
-    .reduce((acc, curr) => acc + curr, 0);
+function getPossibleGamesIds(inputData: string[]): any {
+  function getGameData(
+    input: string
+  ): [id: number, reveals: CubeRevealTuple[]] {
+    const [gameId, games] = input.split(":").map((a) => a.trim());
+    const id = Number(gameId.split(" ")[1]);
 
-  if (red > maxCubes.red || blue > maxCubes.blue || green > maxCubes.green) {
-    return 0;
-  } else {
-    return Number(id);
+    const content = games.split("; ").map((turn) => turn.trim());
+
+    const reveals = content
+      .map((pair) => pair.split(", "))
+      .flat()
+      .map((a) => a.split(" ") as CubeRevealTuple);
+
+    return [id, reveals];
   }
+
+  function isGamePossible(reveal: CubeRevealTuple) {
+    const [value, color] = reveal;
+
+    const possibleTotal = maxCubes[color];
+
+    return value <= possibleTotal || false;
+  }
+
+  const validGamesIds = inputData
+    .map(getGameData)
+    .filter(
+      ([_, reveals]) => !reveals.some((reveal) => !isGamePossible(reveal))
+    )
+    .map(([id]) => id);
+
+  return validGamesIds;
 }
 
 export function solve(): number {
-  const possibleGamesIds = input.map(getPossibleGamesIds);
+  const possibleGamesIds = getPossibleGamesIds(inputData);
   const result = sum(possibleGamesIds);
   return result;
 }
